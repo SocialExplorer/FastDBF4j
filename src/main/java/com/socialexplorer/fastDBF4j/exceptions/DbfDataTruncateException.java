d4f596c8d5148aadd411ac777b84cac63d579d3b
Maida Arnautovic <maida.arnautovic@gmail.com>
2014-11-12 14:57:01 +0100

    Reorganize files.

---
+++ src/main/java/com/socialexplorer/fastDBF4j/exceptions/DbfDataTruncateException.java
@@ -0,0 +1,9 @@
+package com.socialexplorer.fastDBF4j.exceptions;
+
+import java.lang.Exception;
+
+public class DbfDataTruncateException extends Exception {
+    public DbfDataTruncateException(String message) {
+        super(message);
+    }
+}
